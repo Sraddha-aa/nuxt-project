fadc158781c6c7674102db231389217f7d3c3d4a
Sraddha-aa <sraddha@mozilor.com>
2023-09-01 17:06:18 +0530

    git

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtWelcome />
-    <!-- Start cookieyes banner --> <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/108700199d84132d886c6833/script.js"></script> <!-- End cookieyes banner -->
+   <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/108700199d84132d886c6833/script.js"></script> 
   </div>
 </template>
