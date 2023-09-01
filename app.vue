6a3db0db0c846cf4880d2b7af8b34ab6e074b63e
Sraddha-aa <sraddha@mozilor.com>
2023-09-01 16:58:05 +0530

    project

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <NuxtWelcome />
+    <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/73fca828dc7019b627b807fa/script.js"></script> 
   </div>
 </template>
